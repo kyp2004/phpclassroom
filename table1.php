70932776300572f106eac2879d508d8e13ec2a20
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-11-16 09:08:13 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,15 @@
+<?php
+  echo "<table border='1'>"; //tagเปิด
+    
+  for($i=0;$i<10;$i++){
+    echo "<tr>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "</tr>";  
+  }
+  
+  echo "</table>"; //tagปิด
+
+?>
