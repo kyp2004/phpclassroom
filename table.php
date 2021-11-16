72422a0385e057e0509b3409f17b4764eccf8b6a
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-11-16 08:51:23 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,19 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>column 1</td>
+        <td>column 2</td>
+        <td>column 3</td>
+        <td>column 4</td>
+      </tr>
+      <tr>
+        <td>column 1</td>
+        <td>column 2</td>
+        <td>column 3</td>
+        <td>column 4</td>
+      </tr>
+    </table>
+  </body>
+</html>
