3af84543ab04157bfa44f13b27426ee0654a6de2
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-11-16 09:30:29 +0700

    Create table2.php

---
+++ table2.php
@@ -0,0 +1,15 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <?php
+        $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์ที่ 1</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
