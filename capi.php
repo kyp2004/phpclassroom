9b8a4884d22d814e0802825f4b3cfaae01328314
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-12-07 09:20:03 +0700

    Update capi.php

--- capi.php
+++ capi.php
@@ -1,7 +1,6 @@
 <?php
-
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
-  var_dump($data);
-
+  echo "ยอดผู้ป่วยวันนี้ : ".$data[0]->new_case."<br>";
+  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_case."<br>";
 ?>
