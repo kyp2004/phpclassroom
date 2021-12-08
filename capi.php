cc2e84f6e5610cbb6aa7c846b276df487077dc08
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-12-08 14:48:42 +0700

    Update capi.php

--- capi.php
+++ capi.php
@@ -1,6 +1,14 @@
 <?php
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
-  echo "ยอดผู้ป่วยวันนี้ : ".$data[0]->new_case."<br>";
-  echo "ยอดผู้ป่วยรวม : ".$data[0]->total_case."<br>";
+  echo "วันที่ : ".$data[0]->txn_date ."<br>";
+  echo "ผู้ป่วยใหม่ : ".$data[0]->new_case ."<br>";
+  echo "ผู้ป่วยรวม : ".$data[0]->total_case."<br>";
+  echo "ผู้ป่วยใหม่ในประเทศ : ".$data[0]->new_case_excludeabroad."<br>";
+  echo "ผู้ป่วยรวมในประเทศ : ".$data[0]->total_case_excludeabroad ."<br>";
+  echo "ผู้เสียชีวิตใหม่ : ".$data[0]->new_death ."<br>";
+  echo "ผู้เสียชีวิตรวม : ".$data[0]->total_death ."<br>";
+  echo "หายป่วยใหม่ : ".$data[0]->new_recovered ."<br>";
+  echo "หายป่วยรวม : ".$data[0]->total_recovered ."<br>";
+ 
 ?>
