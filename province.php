a12607a71511a32a57504f72c60f4f07314655c8
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-12-21 19:30:36 +0700

    Update province.php

--- province.php
+++ province.php
@@ -6,7 +6,7 @@
   echo "<center><h2>รายงานสถานการโควิด-19 แยกจังหวัด</h2></center>";
   echo '<body style="background-color:LightCoral">';
   echo "<center><table border='1'>";
-  echo "<tr><td style='background-color:Salmon'><b>ลำดับ</b></td><td style='background-color:Salmon'><b>จังหวัด</b></td><td style='background-color:MistyRose'><b>ผู้ป่วยใหม่</b></td><td style='background-color:LightCoral'><b>ผู้ป่วยรวม</b></td><td style='background-color:LightCoral'><b>ผู้ป่วยใหม่ในประเทศ</b></td><td style='background-color:MistyRose'><b>ผู้ป่วยรวมในประเทศ</b></td><td style='background-color:MistyRose'><b>ผู้เสียชีวิตใหม่</b></td><td style='background-color:MistyRose'><b>ผู้เสียชีวิตรวม</b></td></tr>";
+  echo "<tr><td style='background-color:PaleVioletRed'><b>ลำดับ</b></td><td style='background-color:Salmon'><b>จังหวัด</b></td><td style='background-color:MistyRose'><b>ผู้ป่วยใหม่</b></td><td style='background-color:LightCoral'><b>ผู้ป่วยรวม</b></td><td style='background-color:LightCoral'><b>ผู้ป่วยใหม่ในประเทศ</b></td><td style='background-color:MistyRose'><b>ผู้ป่วยรวมในประเทศ</b></td><td style='background-color:MistyRose'><b>ผู้เสียชีวิตใหม่</b></td><td style='background-color:MistyRose'><b>ผู้เสียชีวิตรวม</b></td></tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td style='background-color:Seashell'>";
