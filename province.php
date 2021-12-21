897f2a465689345c6a80d64ee4542becf4d2868f
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-12-21 14:53:53 +0700

    Update province.php

--- province.php
+++ province.php
@@ -4,7 +4,8 @@
   $data = json_decode($json);
 
   echo "<center><h3>รายงานสถานการโควิด-19 แยกจังหวัด</h3></center>";
-  echo '<body style="background-color:PeachPuff">';
+  echo '<body style="background-color:lavender
+  ">';
   echo "<center><table border='1'>";
   echo "<tr><td><b>ลำดับ</b></td><td><h4>จังหวัด</h4></td><td><h4>ผู้ป่วยใหม่</h4></td><td><h4>ผู้ป่วยรวม</h4></td><td><h4>ผู้ป่วยใหม่ในประเทศ</h4></td><td><h4>ผู้ป่วยรวมในประเทศ</h4></td><td><h4>ผู้เสียชีวิตใหม่</h4></td><td><h4>ผู้เสียชีวิตรวม</h4></td><td><h4>วันที่อัพเดท</h4></td></tr>";
   foreach($data as $key=>$val){
