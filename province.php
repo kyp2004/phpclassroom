ba29ae2fea23ae1cf9ef6e6decfbdf7442644d94
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2021-12-21 09:30:04 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,7 +3,8 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  echo "<table border='20'>";
+  echo "<table border='1'>";
+  echo "<tr><td>รายงานสถานการโควิด-19 แยกจังหวัด</td></tr>";
   echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>ผู้ป่วยใหม่</td><td>ผู้ป่วยรวม</td><td>ผู้ป่วยใหม่ในประเทศ</td><td>ผู้ป่วยรวมในประเทศ</td><td>ผู้เสียชีวิตใหม่</td><td>ผู้เสียชีวิตรวม</td><td>วันที่อัพเดท</td></tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
