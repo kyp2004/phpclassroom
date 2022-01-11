6a7641f52e7727820a3a1e3e73242a6b71e3ecc8
kyp2004 <89735904+kyp2004@users.noreply.github.com>
2022-01-11 09:06:39 +0700

    Create style.php

---
+++ style.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+   <style>
+     .g{
+        background-color: green;
+     }
+     #box1{
+        width:50px;
+        height:50px;
+     }
+     #box2{
+        width:100px;
+        height:50px;
+     }
+   </style>
+  </head>
+  <body>
+    <div class="g" id="box1"></div>
+    <br><br><br>
+    <div  class="g" id="box2"> </div>
+  </body>
+</html>
+© 2022 GitHub, Inc.
+Terms
